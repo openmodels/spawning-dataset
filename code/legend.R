b05abdf8c8fae884cf701e9dee3cf777fb1128d2
James Rising <jarising@gmail.com>
2023-03-08 13:39:36 -0500

    core files

---
+++ code/legend.R
@@ -0,0 +1,55 @@
+setwd("~/Dropbox/Spawning ProCreator")
+
+library(PBSmapping)
+library(ggplot2)
+
+shp = importShapefile("dataset/GeoSpawn.shp")
+polydata <- attr(shp, 'PolyData')
+
+shp.coast <- importShapefile("~/data/shapefiles/ne_10m_coastline/ne_10m_coastline.shp")
+shp.adm0 <- importShapefile("~/data/shapefiles/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp")
+
+## plot(0, xaxt='n', yaxt='n', bty='n', pch='', ylab='', xlab='', xlim=c(0, 1), ylim=c(0, 1))
+## for (ii in 1:100) {
+##     y0 = log10(ii) / 2
+##     rect(.25, .25 + y0 / 2, .5, .75, col="#0000001a", border=NA)
+##     lines(c(.25, .25), c(.25 + y0 / 2, .75), col="#00000033")
+##     lines(c(.5, .5), c(.25 + y0 / 2, .75), col="#00000033")
+## }
+
+for (month in c('djf', 'mam', 'jja', 'son')) {
+    grid <- expand.grid(X=seq(-180 + .05, 180 - .05, by=.1),
+                    Y=seq(-78 + .05, 81 - .05, by=.1))
+    grid$count <- 0
+    grid$EID <- 1:nrow(grid)
+    grid <- as.EventData(grid, projection=1)
+
+    for (pid in unique(shp$PID)) {
+        if (!is.na(month)) {
+            if (month %in% c('djf', 'mam', 'jja', 'son')) {
+                months <- list('djf'=c(12, 1, 2), 'mam'=3:5, 'jja'=6:8, 'son'=9:11)[[month]]
+                if (all(is.na(polydata[pid, months + 1]) | polydata[pid, months + 1] == 0))
+                    next
+            } else if (is.na(polydata[pid, month + 1]) || polydata[pid, month + 1] == 0)
+                next
+        }
+        print(c(month, pid))
+        found <- findPolys(grid, subset(shp, PID == pid), maxRows=nrow(grid))
+        grid$count[found$EID] <- grid$count[found$EID] + 1
+    }
+    if (!is.na(month))
+        grid$count <- pmin(grid$count, 100)
+
+    ggplot(subset(grid, count > 0), aes(X, Y)) +
+        coord_fixed(ylim=c(-78, 81)) +
+        geom_polygon(data=shp.adm0, aes(group=paste(PID, SID)), colour='#c3b840', fill='#936d24', size=.1) +
+        geom_raster(aes(fill=count)) +
+        geom_path(data=shp.coast, aes(group=paste(PID, SID)), colour='#c3b840', size=.1) +
+        scale_fill_gradient("Spawning regions:", low='#9ecae1', high='#08306b', trans='log10', breaks=c(1, 3, 10, 30, 100), limits=c(1, max(100, max(grid$count)))) + scale_x_continuous(NULL, expand=c(0, 0)) +
+        scale_y_continuous(NULL, expand=c(0, 0)) + theme_bw() +
+        theme(legend.position="bottom", legend.key.width=unit(1, 'cm'))
+    if (is.na(month))
+        ggsave("figures/global.png", width=13, height=6.5)
+    else
+        ggsave(paste0("figures/month", month, ".png"), width=13, height=6.5)
+}
---
+++ code/spawning-geoprep.R
@@ -0,0 +1,135 @@
+setwd("~/Dropbox/Climate Change Fish Nets/code/")
+datapath <- "~/research/fishnets/"
+
+library(maps)
+library(dplyr)
+
+df <- read.csv("spawning-records.csv")
+df$months <- rowSums(df[, 1:12] > 0, na.rm=T)
+df$score <- ((rowSums(!is.na(df[, 1:12])) > 0) + 1) / 2
+df$region <- paste(df$country, df$localities, sep=' :: ')
+df$genus <- sapply(df$species, function(x) strsplit(as.character(x), " ")[[1]][1])
+
+## For calculating the weights
+catch <- read.csv(paste0(datapath, "saudata/catches.csv"), header=F)
+df.catch <- data.frame(key=catch$V1, catch=rowSums(catch[, -1], na.rm=T))
+df.catch2 <- df.catch %>% group_by(key) %>% summarize(catch=sum(catch))
+value <- read.csv(paste0(datapath, "saudata/values.csv"), header=F)
+df.value <- data.frame(key=value$V1, value=rowSums(value[, -1], na.rm=T))
+df.value2 <- df.value %>% group_by(key) %>% summarize(value=sum(value))
+
+df.saueez <- read.csv(paste0(datapath, "saudata/species.csv"))
+df.sau <- df.saueez[!duplicated(df.saueez$key), -1] %>% left_join(df.catch2) %>% left_join(df.value2)
+
+geos1 <- read.csv("../codepy/localities-geonames.csv")
+geos2 <- read.csv("../codepy/localities-arcgis.csv")
+
+regionslist <- unique(df$region)
+
+results <- data.frame()
+allrects <- data.frame()
+for (rr in 1:length(regionslist)) {
+    region <- regionslist[rr]
+
+    ## Calculate the weight for this region
+    part.catch <- 0
+    part.value <- 0
+    speclist.catch <- list()
+    for (species in unique(df$species[df$region == region])) {
+        genus.scale <- 1
+        row <- df.sau[df.sau$scientific == as.character(species),]
+        if (nrow(row) == 0) {
+            genus <- strsplit(as.character(species), " ")[[1]][1]
+            row <- df.sau[df.sau$scientific == genus,]
+            genus.scale <- 1 / length(unique(df$species[df$genus == genus]))
+            if (nrow(row) == 0)
+                next
+        }
+        portion <- genus.scale * sum(df$score[df$species == species & df$region == region]) / sum(df$score[df$species == species])
+        part.catch <- part.catch + row$catch * portion
+        part.value <- part.value + row$value * portion
+        speclist.catch[[species]] <- row$catch * portion
+    }
+
+    parts <- strsplit(region, " :: ")[[1]]
+    country <- parts[1]
+    localities <- parts[2]
+
+    specstr <- paste(sapply(names(speclist.catch), function(species) paste0(species, " (", round(100 * speclist.catch[[species]] / part.catch), "%)")), collapse=", ")
+
+    master.rect1 <- rep(NA, 4) # WSEN
+    master.rect2 <- rep(NA, 4) # WSEN
+
+    ## Create the map with geocodings
+
+    pdf(paste0("~/Dropbox/Spawning ProCreator/maps/map", rr, ".pdf"), width=6, height=3)
+    par(rep(0, 4))
+    map("world", mar=rep(0, 4))
+
+    ## Plot goes 1
+    g1row <- geos1[((!is.na(geos1$country) & geos1$country == country) | (is.na(geos1$country) & country == "NA")) &
+                   ((!is.na(geos1$localities) & geos1$localities == localities) | (is.na(geos1$localities) & localities == "NA")),]
+    if (nrow(g1row) > 0) {
+        if (is.na(g1row$ne_lon) || g1row$ne_lon - g1row$sw_lon < .25 || g1row$ne_lat - g1row$sw_lat < .25) {
+            if (!is.na(g1row$lon)) {
+                points(g1row$lon, g1row$lat, col=2)
+                master.rect1 <- c(g1row$lon - .25, g1row$lat - .25, g1row$lon + .25, g1row$lat + .25)
+            }
+            if (!is.na(g1row$ne_lon)) {
+                points((g1row$ne_lon + g1row$sw_lon) / 2, (g1row$ne_lat + g1row$sw_lat) / 2, col=2)
+                master.rect1 <- c(g1row$sw_lon - .125, g1row$sw_lat - .125, g1row$ne_lon + .125, g1row$ne_lat + .125)
+            }
+        } else if (g1row$ne_lon < g1row$sw_lon) {
+            rect(g1row$ne_lon, g1row$ne_lat, -300, g1row$sw_lat, border=2)
+            rect(300, g1row$ne_lat, g1row$sw_lon, g1row$sw_lat, border=2)
+            master.rect1 <- c(-180, g1row$sw_lat, 180, g1row$ne_lat)
+        } else {
+            rect(g1row$ne_lon, g1row$ne_lat, g1row$sw_lon, g1row$sw_lat, border=2)
+            master.rect1 <- c(g1row$sw_lon, g1row$sw_lat, g1row$ne_lon, g1row$ne_lat)
+        }
+    }
+
+    ## Plot goes 2
+    g2row <- geos2[((!is.na(geos2$country) & geos2$country == country) | (is.na(geos2$country) & country == "NA")) &
+                   ((!is.na(geos2$localities) & geos2$localities == localities) | (is.na(geos2$localities) & localities == "NA")),]
+    if (nrow(g2row) > 0 && !is.na(g2row$confidence)) {
+        if (is.na(g2row$ne_lon) || g2row$ne_lon - g2row$sw_lon < .25 || g2row$ne_lat - g2row$sw_lat < .25) {
+            if (!is.na(g2row$lon)) {
+                points(g2row$lon, g2row$lat, col=3)
+                master.rect2 <- c(g2row$sw_lon - .125, g2row$sw_lat - .125, g2row$ne_lon + .125, g2row$ne_lat + .125)
+            }
+        } else if (g2row$ne_lon < g2row$sw_lon) {
+            rect(g2row$ne_lon, g2row$ne_lat, -300, g2row$sw_lat, border=3)
+            rect(300, g2row$ne_lat, g2row$sw_lon, g2row$sw_lat, border=3)
+            master.rect2 <- c(-180, g2row$sw_lat, 180, g2row$ne_lat)
+        } else {
+            rect(g2row$ne_lon, g2row$ne_lat, g2row$sw_lon, g2row$sw_lat, border=3)
+            master.rect2 <- c(g2row$sw_lon, g2row$sw_lat, g2row$ne_lon, g2row$ne_lat)
+        }
+    }
+
+    dev.off()
+
+    ## Create the Map Tool link
+
+    master.rect <- c(min(master.rect1[1], master.rect2[1]), min(master.rect1[2], master.rect2[2]),
+                     max(master.rect1[3], master.rect2[3]), max(master.rect1[4], master.rect2[4]))
+    if (is.na(master.rect[1]))
+        url <- ""
+    else
+        url <- paste0("https://iridl.ldeo.columbia.edu/maproom/Global/Ocean_Temp/Annual_Cycle_Max.html?bbox=bb%3A", master.rect[1], "%3A", master.rect[2], "%3A", master.rect[3], "%3A", master.rect[4], "%3Abb")
+
+    ## Create the final row in the spreadsheet
+
+    results <- rbind(results, data.frame(country, localities, part.catch, part.value, species=specstr, geocode=paste0("https://www.dropbox.com/home/Spawning%20ProCreator/maps?preview=map", rr, ".pdf"), geotool=url))
+    allrects <- rbind(allrects, data.frame(country, localities, which='red', west=master.rect1[1], south=master.rect1[2], east=master.rect1[3], north=master.rect1[4]),
+                      data.frame(country, localities, which='green', west=master.rect2[1], south=master.rect2[2], east=master.rect2[3], north=master.rect2[4]))
+}
+
+## Order by part.catch
+results <- results[order(results$part.catch, decreasing=T),]
+results$verdict <- ""
+results$notes <- ""
+
+write.csv(results, "~/Dropbox/Spawning ProCreator/master.csv", row.names=F)
+write.csv(allrects, "allrects.csv", row.names=F)
