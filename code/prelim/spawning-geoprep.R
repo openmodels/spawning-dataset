025eb20acedc20b2dd6b268c6da78297bb59016f
James Rising <jarising@gmail.com>
2023-03-22 20:59:22 -0400

    remove SAU data

--- code/prelim/spawning-geoprep.R
+++ code/prelim/spawning-geoprep.R
@@ -1,6 +1,8 @@
 library(maps)
 library(dplyr)
 
+do.sauweights <- F # If T, need catch and value in saudata
+
 dropbox.path <- "~/Dropbox/Spawning ProCreator/maps"
 dropbox.url <- "https://www.dropbox.com/home/Spawning%20ProCreator/maps"
 
@@ -10,16 +12,20 @@ df$score <- ((rowSums(!is.na(df[, 1:12])) > 0) + 1) / 2
 df$region <- paste(df$country, df$localities, sep=' :: ')
 df$genus <- sapply(df$species, function(x) strsplit(as.character(x), " ")[[1]][1])
 
-## For calculating the weights
-catch <- read.csv("inputs/saudata/catches.csv", header=F)
-df.catch <- data.frame(key=catch$V1, catch=rowSums(catch[, -1], na.rm=T))
-df.catch2 <- df.catch %>% group_by(key) %>% summarize(catch=sum(catch))
-value <- read.csv("inputs/saudata/values.csv", header=F)
-df.value <- data.frame(key=value$V1, value=rowSums(value[, -1], na.rm=T))
-df.value2 <- df.value %>% group_by(key) %>% summarize(value=sum(value))
-
 df.saueez <- read.csv("inputs/saudata/species.csv")
-df.sau <- df.saueez[!duplicated(df.saueez$key), -1] %>% left_join(df.catch2) %>% left_join(df.value2)
+if (do.sauweights) {
+    ## For calculating the weights
+    catch <- read.csv("inputs/saudata/catches.csv", header=F)
+    df.catch <- data.frame(key=catch$V1, catch=rowSums(catch[, -1], na.rm=T))
+    df.catch2 <- df.catch %>% group_by(key) %>% summarize(catch=sum(catch))
+    value <- read.csv("inputs/saudata/values.csv", header=F)
+    df.value <- data.frame(key=value$V1, value=rowSums(value[, -1], na.rm=T))
+    df.value2 <- df.value %>% group_by(key) %>% summarize(value=sum(value))
+
+    df.sau <- df.saueez[!duplicated(df.saueez$key), -1] %>% left_join(df.catch2) %>% left_join(df.value2)
+} else {
+    df.sau <- df.saueez[!duplicated(df.saueez$key), -1]
+}
 
 geos1 <- read.csv("inputs/localities-geonames.csv")
 geos2 <- read.csv("inputs/localities-arcgis.csv")
@@ -31,31 +37,37 @@ allrects <- data.frame()
 for (rr in 1:length(regionslist)) {
     region <- regionslist[rr]
 
-    ## Calculate the weight for this region
-    part.catch <- 0
-    part.value <- 0
-    speclist.catch <- list()
-    for (species in unique(df$species[df$region == region])) {
-        genus.scale <- 1
-        row <- df.sau[df.sau$scientific == as.character(species),]
-        if (nrow(row) == 0) {
-            genus <- strsplit(as.character(species), " ")[[1]][1]
-            row <- df.sau[df.sau$scientific == genus,]
-            genus.scale <- 1 / length(unique(df$species[df$genus == genus]))
-            if (nrow(row) == 0)
-                next
+    if (do.sauweights) {
+        ## Calculate the weight for this region
+        part.catch <- 0
+        part.value <- 0
+        speclist.catch <- list()
+        for (species in unique(df$species[df$region == region])) {
+            genus.scale <- 1
+            row <- df.sau[df.sau$scientific == as.character(species),]
+            if (nrow(row) == 0) {
+                genus <- strsplit(as.character(species), " ")[[1]][1]
+                row <- df.sau[df.sau$scientific == genus,]
+                genus.scale <- 1 / length(unique(df$species[df$genus == genus]))
+                if (nrow(row) == 0)
+                    next
+            }
+            portion <- genus.scale * sum(df$score[df$species == species & df$region == region]) / sum(df$score[df$species == species])
+            part.catch <- part.catch + row$catch * portion
+            part.value <- part.value + row$value * portion
+            speclist.catch[[species]] <- row$catch * portion
         }
-        portion <- genus.scale * sum(df$score[df$species == species & df$region == region]) / sum(df$score[df$species == species])
-        part.catch <- part.catch + row$catch * portion
-        part.value <- part.value + row$value * portion
-        speclist.catch[[species]] <- row$catch * portion
     }
 
     parts <- strsplit(region, " :: ")[[1]]
     country <- parts[1]
     localities <- parts[2]
 
-    specstr <- paste(sapply(names(speclist.catch), function(species) paste0(species, " (", round(100 * speclist.catch[[species]] / part.catch), "%)")), collapse=", ")
+    if (do.sauweights) {
+        specstr <- paste(sapply(names(speclist.catch), function(species) paste0(species, " (", round(100 * speclist.catch[[species]] / part.catch), "%)")), collapse=", ")
+    } else {
+        specstr <- paste(unique(df$species[df$region == region]), collapse=", ")
+    }
 
     master.rect1 <- rep(NA, 4) # WSEN
     master.rect2 <- rep(NA, 4) # WSEN
@@ -120,14 +132,19 @@ for (rr in 1:length(regionslist)) {
         url <- paste0("https://iridl.ldeo.columbia.edu/maproom/Global/Ocean_Temp/Annual_Cycle_Max.html?bbox=bb%3A", master.rect[1], "%3A", master.rect[2], "%3A", master.rect[3], "%3A", master.rect[4], "%3Abb")
 
     ## Create the final row in the spreadsheet
-
-    results <- rbind(results, data.frame(country, localities, part.catch, part.value, species=specstr, geocode=paste0(dropbox.url, "?preview=map", rr, ".pdf"), geotool=url))
+    if (do.sauweights) {
+        results <- rbind(results, data.frame(country, localities, part.catch, part.value, species=specstr, geocode=paste0(dropbox.url, "?preview=map", rr, ".pdf"), geotool=url))
+    } else {
+        results <- rbind(results, data.frame(country, localities, species=specstr, geocode=paste0(dropbox.url, "?preview=map", rr, ".pdf"), geotool=url))
+    }
     allrects <- rbind(allrects, data.frame(country, localities, which='red', west=master.rect1[1], south=master.rect1[2], east=master.rect1[3], north=master.rect1[4]),
                       data.frame(country, localities, which='green', west=master.rect2[1], south=master.rect2[2], east=master.rect2[3], north=master.rect2[4]))
 }
 
-## Order by part.catch
-results <- results[order(results$part.catch, decreasing=T),]
+if (do.sauweights) {
+    ## Order by part.catch
+    results <- results[order(results$part.catch, decreasing=T),]
+}
 results$verdict <- ""
 results$notes <- ""
 
